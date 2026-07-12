b55f204e0d460d7eb2a52e6c551165da7c846006
Andrey Zhidkov <mr.goodhood@gmail.com>
2026-07-12 21:08:53 +0300

    01_basic

---
+++ 01_python_basics/first_ml.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+numbers = np.array([1, 2, 3, 4, 5])
+
+data =  {
+    "age": [25, 30, 35],
+    "salary": [50000, 70000, 90000]
+}
+
+df = pd.DataFrame(data)
+
+matrix = np.array([
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+])
