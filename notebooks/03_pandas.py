e1e97a4207efc937b4a27b3c3c3ab6dfa817528b
Andrey Zhidkov <mr.goodhood@gmail.com>
2026-07-14 00:28:56 +0300

    03_pandas

---
+++ notebooks/03_pandas.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    "Name": ["Andrey", "Ivan", "Anna"],
+    "Age": [25, 30, 22],
+    "Height": [180, 175, 190]
+}
+
+# df = pd.DataFrame(data)
+#
+# print(df["Height"] > 179)
+
+data_with_missing = {
+    "Name": ["Andrey", "Ivan", "Anna", "Maria"],
+    "Age": [25, 30, np.nan, 28],
+    "Height": [180, np.nan, 190, 168]
+}
+
+df_missing = pd.DataFrame(data_with_missing)
+
+print(df_missing["Age"].fillna(df_missing["Age"].mean()))
+
+
+
+
+
+
